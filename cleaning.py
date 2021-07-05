e1f4434fec8e0a4b1f311e7b11af2eacdad1fa47
leoEspin <35843183+leoEspin@users.noreply.github.com>
2021-07-05 18:05:08 -0400

    added coloring to text

--- cleaning.py
+++ cleaning.py
@@ -2,6 +2,18 @@ import re
 import copy
 import json
 
+# class found here: https://stackoverflow.com/a/287944/10946181
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def make_json(rawtext: list):
     qbank = []
     q_template = {"question": None, "answers": []}
