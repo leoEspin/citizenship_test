9eb6e7edd8c0ca42358106b4715d4d2efe83eba5
leoEspin <35843183+leoEspin@users.noreply.github.com>
2021-07-05 15:41:45 -0400

    almost perfect now. still a question is missing
    had to manually make corrections in raw text

--- cleaning.py
+++ cleaning.py
@@ -2,6 +2,24 @@ import re
 import copy
 import json
 
+def make_json(rawtext: list):
+    qbank = []
+    q_template = {"question": None, "answers": []}
+    current_q = copy.deepcopy(q_template) # need deep copy of template
+
+    while len(rawtext) > 0:
+        line = rawtext.pop(0)
+        check_question = re.match(pattern, line)
+        if check_question:
+            if len(current_q['answers']) > 0:
+                qbank.append(current_q)
+                current_q = copy.deepcopy(q_template) 
+            current_q["question"] = check_question.groups()[1].strip()
+        elif '▪' in line:
+            current_q["answers"].append(re.sub('^[\s▪]+', '', line).strip())
+
+    return  {"Questions": qbank}
+
 with open('question_bank.txt') as raw:
     rawtext = raw.readlines()
 rawtext = [line.replace('\t', '').strip() for line in rawtext]
@@ -9,27 +27,15 @@ rawtext = [line for line in rawtext if len(line) > 0]
 
 cleaner = []
 pattern = '^(\d+\.)(.+)$' # captures whatever comes after the question number dot pattern
-for i in range(len(rawtext)):
-    if rawtext[i][0].isdigit() and not re.match(pattern, rawtext[i]):
-        cleaner.append(rawtext[i] + rawtext[i+1])
-    else:
-        cleaner.append(rawtext[i])
 
-qbank = []
-q_template = {"question": None, "answers": []}
-N = len(rawtext)
-current_q = copy.deepcopy(q_template) # need deep copy of template
-
-for i in range(N):
+while len(rawtext) >0 :
     line = rawtext.pop(0)
-    check_question = re.match(pattern, line)
-    if check_question:
-        if len(current_q['answers']) > 0:
-            qbank.append(current_q)
-            current_q = copy.deepcopy(q_template) 
-        current_q["question"] = check_question.groups()[1].strip()
-    elif '▪' in line:
-        current_q["answers"].append(re.sub('^[\s▪]+', '', line).strip())
+    if line[0].isdigit() and not re.match(pattern, line):
+        cleaner.append(line + rawtext.pop(0))
+    else:
+        cleaner.append(line)
+
+processed = make_json(cleaner)
 
 with open('question_bank.json','w') as output:
-    json.dump({"Questions": qbank}, output, indent = 2)
+    json.dump(processed, output, indent = 2)
